ddd354901fba21bf5bcafc420ce351d088899dbb
Sam S <167921656+SamuelSor@users.noreply.github.com>
2026-07-26 20:16:01 -0400

    Create Day3-Treasure-Island.py

---
+++ Day3-Treasure-Island.py
@@ -0,0 +1,44 @@
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+turn = input("There are two tunnels in front of you. Will you choose the left or right tunnel? ")
+
+if turn == "left":
+    swim = input("You appear at a lake. You can either swim across or go around the long way. Would you like to swim? Y or N?")
+    if swim == "Y":
+        door_color= input("After reaching the other side of the lake, you see 3 colored doors and a sign saying to enter one. What door do you want to enter? blue, red or yellow?")
+
+        if door_color == "yellow":
+            print("You made it to the treasure! You win!!")
+        elif door_color == "blue":
+            print("Eaten by beasts.\nGame Over.")
+        elif door_color == "red":
+            print("Burned by fire.\nGame Over.")
+        else:
+            print("Game Over.")
+    else:
+        print("Attacked by trout.\nGame Over.")
+else:
+    print("The tunnel collapse.\nGame Over.")
