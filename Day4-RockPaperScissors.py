d6e708e3eb4b6fbea75d3d540d1d20678e215670
Sam S <167921656+SamuelSor@users.noreply.github.com>
2026-07-27 00:22:59 -0400

    Create Day4-RockPaperScissors.py

---
+++ Day4-RockPaperScissors.py
@@ -0,0 +1,51 @@
+from filecmp import clear_cache
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+    ROCK
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+    PAPER
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+    SCISSORS
+'''
+hand_signs = [rock, paper, scissors]
+
+user = int(input("ROCK,PAPER,SCISSORS!!!\nEnter 0 for rock, 1 for paper, 2 for scissors: "))
+opponent = random.randint(0, 2)
+if user >=0 and user <= 2:
+    print(f"You chose {hand_signs[user]}")
+    print(f"Computer chose {hand_signs[opponent]}")
+
+if user >2 and user <0:
+    print("Invalid Number: YOU LOSE!")
+elif user == opponent:
+    print("It's a tie.")
+elif user == 0 and opponent == 2:
+    print("You WIN!")
+elif opponent == 0 and user == 2:
+    print("You LOSE!")
+elif user > opponent:
+    print("You win!")
+elif opponent > user:
+    print("You lose!")
