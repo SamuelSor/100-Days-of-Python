0b222a36ded30070420c4fb4993129016180aaeb
Sam S <167921656+SamuelSor@users.noreply.github.com>
2026-07-29 01:24:51 -0400

    Create Day6-Reeborgs-Maze.py

---
+++ Day6-Reeborgs-Maze.py
@@ -0,0 +1,20 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def turn_around():
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if not wall_on_right():
+        turn_right()
+        move()
+    elif not wall_in_front():
+        move()
+    else:
+        turn_left()
+
+        
+    
